deb94b8fbd4e2cf0dd0e43359ceba9500d61798b
PetrPalisek <61236825+PetrPalisek@users.noreply.github.com>
2024-06-05 18:31:37 +0200

    Add files via upload

--- report_M2.R
+++ report_M2.R
@@ -2,7 +2,7 @@ report_M2 <- function(fit_object){
   fit_object <- round(fit_object,3)
   p <- ifelse(fit_object$p == 0, "p < .001", paste0("p = ", fit_object$p, sep = ""))
   paste0("M2", "(", fit_object$df, ")", " ", "=", " ", fit_object$M2,", ",
-         p, 
+         p, ", "
          "RMSEA = ", fit_object$RMSEA, " [", fit_object$RMSEA_5, " ; ", fit_object$RMSEA_95, "]", ", ",
          "TLI = ", fit_object$TLI, ", ",
          "SRMSR = ", fit_object$SRMSR,
