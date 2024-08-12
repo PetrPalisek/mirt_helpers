6007b79125b56a107c511ea71968333aa6c63339
PetrPalisek <61236825+PetrPalisek@users.noreply.github.com>
2024-08-12 16:02:14 +0200

    Add files via upload

--- create_normtable.R
+++ create_normtable.R
@@ -11,13 +11,15 @@ create_normtable <- function(model, group_ids) {
     # Create a data frame for the current group with M and SD
     group_data[[group_id]] <- data.frame(
       M = group_pars[1],
-      SD = sqrt(group_pars[2, 2]),
+      SD = sqrt(group_pars[1, 2]),
       group = group_id
     )
   }
   
   # Combine all group data into a single data frame
   norm_table <- bind_rows(group_data)
+  
+  norm_table[1,2] <- 1
   
   return(norm_table)
 }
